185fec4ea8b138b794a14e192172d8ee6499a49b
Ivan Bulakhov <i.bulakhov@jino.ru>
2020-05-24 17:11:20 +0500

    add router

---
+++ src/views/Form.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <VFilter :fields="fields"/>
+</div>
+</template>
+
+<script>
+import VFilter from '../components/VFilter'
+
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  components: {
+    VFilter
+  },
+  data () {
+    return {
+      fields: [
+        {
+          code: 'field1',
+          caption: 'поле 1',
+          type: 'number'
+        },
+        {
+          code: 'field2',
+          caption: 'поле 3',
+          type: 'string'
+        },
+        {
+          code: 'field4',
+          caption: 'поле 4',
+          type: 'bool'
+        }
+      ]
+
+    }
+  }
+})
+export default class FormView extends Vue {
+}
+</script>
