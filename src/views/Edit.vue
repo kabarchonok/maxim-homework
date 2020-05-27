2cc06f4f9efdd74292880276dfeb6c47454520c7
Ivan Bulakhov <i.bulakhov@jino.ru>
2020-05-27 17:23:54 +0500

    add fields getter

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,15 +1,26 @@
 <template>
-  <VFormBuilder class="FormBuilder"/>
+  <VFormBuilder class="FormBuilder" :fields="fields" @save-field="saveField"/>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import VFormBuilder from '@/components/VFormBuilder'
+import { Field } from '@/utils/types'
 
 @Component({
   components: { VFormBuilder }
 })
-export default class EditView extends Vue {}
+export default class EditView extends Vue {
+  get fields (): Field[] {
+    return this.$store.getters['form/fields']
+  }
+
+  saveField (field: Field) {
+    const action = field.code ? 'form/editField' : 'form/createField'
+
+    this.$store.commit(action, field)
+  }
+}
 </script>
 
 <style scoped>
