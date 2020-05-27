7feacf7c0a48204856e3ebbaaf59cb1ed68d3bc9
Ivan Bulakhov <i.bulakhov@jino.ru>
2020-05-27 13:55:15 +0500

    add fieldHelper

---
+++ src/utils/fieldHelper.ts
@@ -0,0 +1,21 @@
+export const fieldTypes = [
+  { text: 'Текстовое поле', value: 'string' },
+  { text: 'Числовое поле', value: 'number' },
+  { text: 'Логическое поле', value: 'bool' }
+]
+
+export const fieldTypesObj = {
+  string: 'Текстовое поле',
+  number: 'Числовое поле',
+  bool: 'Логическое поле'
+}
+
+export const fieldRules = {
+  qt: 'Больше',
+  lt: 'Меньше',
+  qte: 'Больше или равно',
+  lte: 'Меньше или равно',
+  eq: 'Равно',
+  neq: 'Не равно',
+  like: 'Содержит'
+}
