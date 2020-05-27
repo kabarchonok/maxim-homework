9d23a2ed5662c0c45bf618ae4783ae78839e565a
Ivan Bulakhov <i.bulakhov@jino.ru>
2020-05-27 13:54:43 +0500

    add list

--- src/components/TheNavigation/TheNavigation.vue
+++ src/components/TheNavigation/TheNavigation.vue
@@ -1,6 +1,21 @@
 <template>
   <v-navigation-drawer permanent :mini-variant="drawer" app>
 
+    <v-list>
+      <v-list-item
+        v-for="item in items"
+        v-bind:key="item.name"
+        :to="item.href"
+        :title="item.name"
+      >
+        <v-list-item-icon>
+          <v-icon>{{item.icon}}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{item.name}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
@@ -8,6 +23,14 @@
 import { Vue, Component, Prop } from 'vue-property-decorator'
 
 @Component({
+  data () {
+    return {
+      items: [
+        { name: 'Посмотреть форму', icon: 'list_alt', href: '/form' },
+        { name: 'Редактировать структуру', icon: 'assignment', href: '/edit' }
+      ]
+    }
+  },
   methods: {
     onInput () {
       console.log('input')
