b6093652fdfab752bf2d8459aee8fb8c19cf105a
Ivan Bulakhov <i.bulakhov@jino.ru>
2020-05-27 13:55:38 +0500

    add edit view

---
+++ src/views/Edit.vue
@@ -0,0 +1,20 @@
+<template>
+  <VFormBuilder class="FormBuilder"/>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import VFormBuilder from '@/components/VFormBuilder'
+
+@Component({
+  components: { VFormBuilder }
+})
+export default class EditView extends Vue {}
+</script>
+
+<style scoped>
+.FormBuilder {
+  max-width: 800px;
+  margin: auto;
+}
+</style>
