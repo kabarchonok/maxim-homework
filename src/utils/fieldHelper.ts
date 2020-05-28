063a706cfec3661bdce62171aab620f7152cfefa
Ivan Bulakhov <i.bulakhov@jino.ru>
2020-05-28 19:05:56 +0500

    update interfaces

--- src/utils/fieldHelper.ts
+++ src/utils/fieldHelper.ts
@@ -1,16 +1,20 @@
+interface Interface {
+  [key: string]: string;
+}
+
 export const fieldTypes = [
   { text: 'Текстовое поле', value: 'string' },
   { text: 'Числовое поле', value: 'number' },
   { text: 'Логическое поле', value: 'bool' }
 ]
 
-export const fieldTypesObj = {
+export const fieldTypesObj: Interface = {
   string: 'Текстовое поле',
   number: 'Числовое поле',
   bool: 'Логическое поле'
 }
 
-export const fieldRules = {
+export const fieldRuleLabels: Interface = {
   qt: 'Больше',
   lt: 'Меньше',
   qte: 'Больше или равно',
