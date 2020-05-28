c4cda7d8d2b7c02b54a19b61b910d6f653bd282a
Ivan Bulakhov <i.bulakhov@jino.ru>
2020-05-28 13:05:32 +0500

    fix eslint

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,34 +1,33 @@
 export interface CommonField {
-  code: string
-  caption: string
-  type: string //FIXME: 'number' | 'string' | 'bool'
-  value?: object
+  code: string;
+  caption: string;
+  type: string; // FIXME: 'number' | 'string' | 'bool'
+  value?: object;
 }
-
 
 export interface NumberField extends CommonField {
   value?: {
-    qt?: number
-    lt?: number
-    qte?: number
-    lte?: number
-    eq?: number
-    neq?: number
-  }
+    qt?: number;
+    lt?: number;
+    qte?: number;
+    lte?: number;
+    eq?: number;
+    neq?: number;
+  };
 }
 
 export interface StringField extends CommonField {
   value?: {
-    like?: string
-    eq?: string
-    neq?: string
-  }
+    like?: string;
+    eq?: string;
+    neq?: string;
+  };
 }
 
 export interface BooleanField extends CommonField {
   value?: {
-    eq?: boolean
-  }
+    eq?: boolean;
+  };
 }
 
 export type Field = NumberField | StringField | BooleanField
