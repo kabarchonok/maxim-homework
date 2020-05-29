ffb45a21ef46a0cc951d0b666ecb2cf28a5a717d
Ivan Bulakhov <i.bulakhov@jino.ru>
2020-05-29 11:56:44 +0500

    add empty state

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,5 +1,8 @@
 <template>
-  <VCard class="Form">
+  <VCard
+    v-if="fields.length"
+    class="Form"
+  >
     <VCardTitle>
       <h1 class="title">
         Форма
@@ -10,14 +13,29 @@
       <VFilter :fields="fields" />
     </VCardText>
   </VCard>
+
+  <VEmptyState
+    v-else
+    icon="list_alt"
+  >
+    <div>Похоже, форма пустая</div>
+    <div>
+      Вы можете
+      <RouterLink to="/edit#new">
+        создать
+      </RouterLink> поля и попробовать снова
+    </div>
+  </VEmptyState>
 </template>
 
 <script lang="ts">
 import VFilter from '@/components/VFilter'
 import { Vue, Component } from 'vue-property-decorator'
+import VEmptyState from '@/components/VEmptyState/VEmptyState.vue'
 
 @Component({
   components: {
+    VEmptyState,
     VFilter
   }
 })
